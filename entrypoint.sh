db3acd35f137990ffcd4b9cf5f4cd1c71ca407eb
Anish Agnihotri <contact@anishagnihotri.com>
2019-08-22 16:32:18 -0400

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Stop bash execution if there is an error
 set -e
 
-NEUTRAL_EXIT_CODE=78
+# Setup neutral exit code used to throw an error.
+exit_code=78
 
-echo "this is working"
+# Check if comment includes the '/poll' initiation command.
+echo "Checking for '/poll' command..."
+(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -Fq "/poll") || exit $exit_code
+
+# Check if comment has already been created.
+if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
+	echo "This is not a new comment event!"
+	exit $NEUTRAL_EXIT_CODE
+fi
